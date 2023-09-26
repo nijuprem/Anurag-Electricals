199f9cb7ebb712cbc20538df9746a231b17eef5c
niju.prem <nijuprem7@gmail.com>
2023-09-26 16:03:20 +0530

    Logos Added

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import { HStack, Box, Image } from "@chakra-ui/react";
 import HeadingItems from "./HeadingItems";
-import logo from "../assets/logo.png";
+import logo from "../assets/AE.png";
+import logo1 from "../assets/AE_Logo.png";
 
 const NavBar = () => {
   return (
@@ -17,12 +18,27 @@ const NavBar = () => {
         height={{ md: "5rem", base: "3rem" }}
       >
         <Box>
-          <Image
-            boxSize={"fit-content"}
-            objectFit="cover"
-            src={logo}
-            alt="Logo"
-          />
+          <HStack ml={{ md: 5, base: 7 }}>
+            <Box
+              w={{ md: "4rem", base: "2.7rem" }}
+              mt={{ md: "0", base: "0.7rem" }}
+            >
+              <Image
+                // boxSize={"fit-content"}
+                objectFit="cover"
+                src={logo}
+                alt="Logo"
+              />
+            </Box>
+            <Box w={"11rem"} display={{ base: "none", md: "block" }} ml={2}>
+              <Image
+                // boxSize={"fit-content"}
+                objectFit="cover"
+                src={logo1}
+                alt="Main Logo"
+              />
+            </Box>
+          </HStack>
         </Box>
         <Box marginRight={{ md: "2rem", base: "0.5rem" }}>
           <HStack>
